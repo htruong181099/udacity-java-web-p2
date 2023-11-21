f508acfaabeb25494f3f57ebb4ee71322b5b8861
hoangtruong181099 <hoangtruong181099@gmail.com>
2023-11-21 23:00:46 +0700

    implement vehicle api, price service, eureka server

---
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -0,0 +1,45 @@
+package com.udacity.pricing;
+
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.domain.price.PriceRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+/**
+ * Creates a Spring Boot Application to run the Pricing Service.
+ * TODO: Convert the application from a REST API to a microservice.
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PricingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricingServiceApplication.class, args);
+    }
+
+    private static BigDecimal randomPrice() {
+        return new BigDecimal(ThreadLocalRandom.current().nextDouble(1, 5))
+                .multiply(new BigDecimal(5000d)).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    private static final List<Price> PRICES = LongStream
+            .range(1, 20)
+            .mapToObj(i -> new Price("USD", randomPrice(), i))
+            .collect(Collectors.toList());
+    @Bean
+    CommandLineRunner initDatabase(PriceRepository repository) {
+        return args -> {
+            repository.saveAll(PRICES);
+        };
+    }
+}
