5b3de46974af7adb9042e1b14b1a83c17d91c1db
hoangtruong181099 <hoangtruong181099@gmail.com>
2023-11-22 14:46:49 +0700

    add test case for pricing service

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,29 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.domain.price.PriceRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import java.math.BigDecimal;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+	@Autowired
+	private PriceRepository priceRepository;
 
 	@Test
-	public void contextLoads() {
+	public void TestRepo()  {
+		Price price = new Price("USD", BigDecimal.valueOf(10), 1L);
+		priceRepository.save(price);
+		assertThat(priceRepository.findById(1L).isPresent()).isTrue();
 	}
 
 }
